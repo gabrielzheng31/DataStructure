95a136e2d5e4a2cd9a62de3ea838023d72163b30
Gabriel Zheng <gabrielzheng31@163.com>
2018-11-21 13:43:48 +0000

    DelX2 for 2.2.3.c

--- 2/2.2/2.2.3.c
+++ 2/2.2/2.2.3.c
@@ -4,8 +4,12 @@ typedef struct {
 	int list_size;
 } SqList;
 
-void DelX(SqList *sq_list, int x)
+void DelX1(SqList *sq_list, int x)
 {
+    /*
+     * var k is used to record the remaining
+     * elements' index while i is the original one.
+     */
     int k = 0;
     for(int i = 0; i < sq_list->length; i++)
     {
@@ -16,4 +20,20 @@ void DelX(SqList *sq_list, int x)
         } 
     }
     sq_list->length = k;
+}
+
+void DelX2(SqList *sq_list, int x)
+{
+    /* 
+     * var k is used to record the num of elem x.
+     */
+    int k = 0;
+    for(int i = 0; i < sq_list->length; i++)
+    {
+        if(sq_list->elem[i] == x)
+            k++;
+        else
+            sq_list->elem[i-k] = sq_list->elem[i];
+    }
+    sq_list->length -= k;
 }
