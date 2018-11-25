da6581c566727ce9b3bb293b3aba024b8d60320c
Gabriel Zheng <gabrielzheng31@163.com>
2018-11-25 08:12:13 +0000

    dlinklist

--- 2_LinearList/2.3_DoubleLinkList/double_link_list.c
+++ 2_LinearList/2.3_DoubleLinkList/double_link_list.c
@@ -33,6 +33,20 @@ void InitDLinkList(DLinkList double_link_list, int n)
     tail->next = NULL;
 }
 
+void InitDLinkListFromHead(DLinkList double_link_list, int n)
+{
+    DLNode *p = (DLNode *)malloc(sizeof(DLNode));
+    scanf("%d", &p->data);
+    for(int i = 0; i<n; i++)
+    {
+        p->next = double_link_list->next;
+        if(double_link_list->next != NULL)
+            double_link_list->next->prior = p; 
+        p->prior = double_link_list;
+        double_link_list->next = p;
+    }
+}
+
 void PrintDLinkList(DLinkList double_link_list)
 {
     DLNode *p = double_link_list->next;
@@ -43,3 +57,44 @@ void PrintDLinkList(DLinkList double_link_list)
     }
     putchar('\n');
 }
+
+void InsertDLinkList(DLinkList double_link_list, int i, int elem)
+{
+    while(i < 1)
+        exit(EXIT_FAILURE);
+    DLNode *p = double_link_list;
+    int j = 0;
+    while(p && j<i-1)
+    {
+        p=p->next;
+        j++;
+    }
+    if(!p)
+        exit(EXIT_FAILURE);
+    
+    DLNode *s = (DLNode *)malloc(sizeof(DLNode));
+    s->data = elem;
+    s->next = p->next;
+    p->next->prior = s;
+    s->prior = p;
+    p->next = s; 
+}
+
+void DeleteDLinkListByIndex(DLinkList double_link_list, int i)
+{
+    DLNode *p = double_link_list;
+    int j = 0;
+    while(p->next && j<i-1)
+    {
+        p=p->next;
+        j++; 
+    }    
+    if(!(p->next))
+        exit(EXIT_FAILURE);
+    DLNode *q =  p->next;
+    p->next = q->next;
+    if(q->next != NULL)
+        q->next->prior = p;
+    free(q);
+}
+
---
+++ 2_LinearList/2.3_DoubleLinkList/pseudo_double_link_list.c
@@ -0,0 +1,7 @@
+InitDLinkListFromHead()
+{
+    assign header to tail;
+    moidfy left link domain;
+    pass tail to s;
+    modify tial->next;
+}
